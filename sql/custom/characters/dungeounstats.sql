2ad8eb57c3bf30c9c09c228076480ebcb8b6a740
Juggalo187 <skulyspam@gmail.com>
2022-08-22 11:09:21 -0500

    dungeon stats sql file

---
+++ sql/custom/characters/dungeounstats.sql
@@ -0,0 +1,28 @@
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+-- `characters`.stats_from_dungeons definition
+DROP TABLE IF EXISTS `stats_from_dungeons`;
+
+CREATE TABLE `stats_from_dungeons` (
+  `GUID` int(10) unsigned NOT NULL,
+  `Agility` float NOT NULL DEFAULT '0',
+  `Intellect` float NOT NULL DEFAULT '0',
+  `Spirit` float NOT NULL DEFAULT '0',
+  `Strength` float NOT NULL DEFAULT '0',
+  `Stamina` float NOT NULL DEFAULT '0',
+  `HealingBonus` int(11) unsigned NOT NULL DEFAULT '0',
+  `SpellDBonus` int(11) unsigned NOT NULL DEFAULT '0',
+  `AttackPower` float NOT NULL DEFAULT '0',
+  `RAttackPower` float NOT NULL DEFAULT '0',
+  PRIMARY KEY (`GUID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
